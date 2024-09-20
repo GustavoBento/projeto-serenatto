fff483b500102a348c65561a39601c4b6310de33
GustavoBento <gustavo_grb-@hotmail.com>
2024-09-19 22:38:57 -0300

    ajustes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const inputCheck = document.querySelector('#modo-noturno')
 const elemento = document.querySelector('body')
+const checarInput = document.querySelector('#modo-noturno')
 
-inputCheck.addEventListener('click', () => {
-      const modo = inputCheck.checked ? 'dark' : 'light'
-      elemento.setAttribute("data-bs-theme", modo)
+checarInput.addEventListener('click', () => {
+      const modo = checarInput.checked ? 'dark' : 'light'
+      elemento.setAttribute('data-bs-theme', modo)
 })
